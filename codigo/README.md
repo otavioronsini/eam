b60ff09add078e11c5c64c0bb563ee948aacf73c
Pedro de Carvalho <85931775+Pedro-Garcia-1@users.noreply.github.com>
2026-06-30 22:23:04 -0300

    Delete codigo directory

--- codigo/README.md
+++
@@ -1,6 +0,0 @@
-# Instalar dependências:
-
-```
-flutter pub add platform_maps_flutter sqflite path
-flutter pub get
-```
--- codigo/lib/domain/auth_repository.dart
+++
@@ -1,6 +0,0 @@
-abstract class AuthRepository {
-  Future<String> cadastrar(String email, String senha);
-  Future<String> login(String email, String senha);
-  Future<void> logout();
-  String? get usuarioAtualId;
-}
